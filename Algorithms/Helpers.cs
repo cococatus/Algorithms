5fde182d28d1564a571845a8f9f0686d290c1728
zhanna <bibi909@ya.ru>
2020-11-03 23:56:15 +0300

    itvdn example of bubble sort

---
+++ Algorithms/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class Helpers
+    {
+        static void Main(string[] args) { }
+
+        public static void Swap(int[] items, int left, int right)
+        {
+            if (left != right)
+            {
+                var temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static Algorithms.Helpers;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 4, 6, 5, 8 };
+
+            PrintArray(array);
+            BubbleSort(ref array);
+            PrintArray(array);
+
+            Console.ReadKey();
+        }
+
+        //ITVDN example
+        public static void BubbleSort(ref int[] items)
+        {
+            bool swapped = false;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 1; i < items.Length; i++)
+                {
+                    if(items[i - 1].CompareTo(items[i]) > 0)
+                    {
+                        Swap(items, i - 1, i);
+                        swapped = true;
+                    }
+                }
+            }
+            while (swapped != false);
+        }
+    }    
+}
