300b6ca0018418bc05b498f506b5cb875b127cd8
zhanna <bibi909@ya.ru>
2020-11-04 11:32:05 +0300

    Insertion sort

--- Algorithms/Helpers.cs
+++ Algorithms/Helpers.cs
@@ -20,6 +20,35 @@ namespace Algorithms
             }
         }
 
+        public static int FindInsertionIndex(int[] items, int valueToInsert)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].CompareTo(valueToInsert) > 0)
+                    return i;
+            }
+
+            throw new InvalidOperationException($"Index not found. There is no value greater than {valueToInsert}");
+        }
+
+        /*
+         * 1. Save value at index 'to' 
+         * 2. Insert value 'from' at index 'to'
+         * 3. Shift array to the right from 'to'+1
+         * 4. Place saved value at 'to'+1
+         */
+        public static void Insert(int[] items, int to, int from)
+        {
+            int temp = items[to];
+            items[to] = items[from];
+
+            //going backwards from 'from' index to 'to'+1
+            for (int i = from; i > to; i--)
+                items[i] = items[i - 1];
+
+            items[to + 1] = temp;
+        }
+
         public static void PrintArray(int[] array)
         {
             foreach (var item in array)
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using static Algorithms.Helpers;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 4, 6, 5, 8 };
+            int[] array1 = { 6, 1, 2, 3, 4, 5, 0 };
+
+            PrintArray(array1);
+            MyInsertionSort(array1);
+            PrintArray(array1);
+
+            Console.ReadKey();
+        }
+
+        //ITVDN example
+        public static void InsertionSort(int[] items)
+        {
+            int sortedRangeEndIndex = 1;
+
+            while(sortedRangeEndIndex < items.Length)
+            {
+                if(items[sortedRangeEndIndex].CompareTo(items[sortedRangeEndIndex - 1]) < 0)
+                {
+                    int insertIndex = FindInsertionIndex(items, items[sortedRangeEndIndex]);
+                    Insert(items, insertIndex, sortedRangeEndIndex);
+                }
+
+                sortedRangeEndIndex++;
+            }
+        }
+
+        public static void MyInsertionSort(int[] items)
+        {
+            for (int i = 1; i < items.Length; i++)
+            {
+                if(items[i].CompareTo(items[i - 1]) < 0)
+                {
+                    var insertIndex = FindInsertionIndex(items, items[i]);
+                    Insert(items, insertIndex, i);
+                }
+            }
+        }
+    }
+}
