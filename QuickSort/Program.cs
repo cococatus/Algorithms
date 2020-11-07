3ba1ac18a57802030cd74fbf2d47c09689fdedee
zhanna <bibi909@ya.ru>
2020-11-07 09:53:05 +0300

    Quick sort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static Algorithms.Helpers;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 7, 4, 4, 6, 5, 8 };
+            int[] array1 = { 6, 1, 2, 3, 4, 5, 0 };
+
+            PrintArray(array);
+            QuickSort(array, 0, array.Length - 1);
+            PrintArray(array);
+
+            Console.ReadKey();
+        }
+
+        //ITVDN example
+        public static void QuickSort(int[] items, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = items[(left + right) >> 1]; // >> is the same as /2 but for bits
+
+            while(i <= j)
+            {
+                while(items[i] < pivot)
+                {
+                    i++;
+                }
+
+                while(items[j] > pivot)
+                {
+                    j--;
+                }
+
+                if(i <= j)
+                {
+                    Swap(items, i, j);
+                    i++;
+                    j--;
+                }
+            }
+
+            if(left < j)
+            {
+                QuickSort(items, left, j);
+            }
+
+            if(i < right)
+            {
+                QuickSort(items, i, right);
+            }
+        }
+    }
+}
